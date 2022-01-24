0d1bc900d536ffb6782b3df71e36922a53db3508
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-01-24 19:44:31 -0300

    Finalizando preenchimento do formulário de teste

---
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -0,0 +1,47 @@
+
+describe('Cadastro', () => {
+    it('Usuário deve se tornar um entregador', () => {
+        cy.viewport(1920,1080);
+        cy.visit('https://buger-eats.vercel.app');
+
+        cy.get('a[href="/deliver"]').click();
+        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas'); // checkpoint
+
+        var entregador = {
+            nome: 'Vinicius Marinho',
+            cpf: '11111111111',
+            email: 'vinicius@vinicius.com',
+            whatsapp: '11123412345',
+            endereco: {
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '1000',
+                complemento: 'Apto 142',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'images/cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome);
+        cy.get('input[name="cpf"]').type(entregador.cpf);
+        cy.get('input[name="email"]').type(entregador.email);
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep);
+        cy.get('input[type=button][value="Buscar CEP"]').click();
+
+        cy.get('input[name="address"]').should('have.value',entregador.endereco.rua);
+        cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro);
+        cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf);
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero);
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click();
+
+        cy.get('input[accept^="image"]').attachFile(entregador.cnh);
+
+    });
+});
