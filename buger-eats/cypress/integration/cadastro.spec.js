e1f8f7c8f2acd603b65ab68e12cc23a82757f04b
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-01-25 19:11:21 -0300

    Passando o projeto para o padrão de Page Objects

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,12 +1,8 @@
+import SignupPage from '../pages/SignupPage';
+
 
 describe('Cadastro', () => {
     it('Usuário deve se tornar um deliver', () => {
-        cy.viewport(1920,1080);
-        cy.visit('https://buger-eats.vercel.app');
-
-        cy.get('a[href="/deliver"]').click();
-        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas'); // checkpoint
-
         var deliver = {
             name: 'Vinicius Marinho',
             cpf: '00000014141',
@@ -24,39 +20,17 @@ describe('Cadastro', () => {
             cnh: 'images/cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name);
-        cy.get('input[name="cpf"]').type(deliver.cpf);
-        cy.get('input[name="email"]').type(deliver.email);
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+        var signup = new SignupPage();
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
+        signup.go();
+        signup.fillForm(deliver);
+        signup.submit();
 
-        cy.get('input[name="address"]').should('have.value',deliver.address.street);
-        cy.get('input[name="district"]').should('have.value',deliver.address.district);
-        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state);
-
-        cy.get('input[name="address-number"]').type(deliver.address.number);
-        cy.get('input[name="address-details"]').type(deliver.address.details);
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click();
-
-        cy.get('input[accept^="image"]').attachFile(deliver.cnh);
-
-        cy.get('form button[type="submit"]').click();
-
-        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage);
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
+        signup.modalContentShouldBe(expectedMessage);
 
     });
     it('CPF Incorreto', () => {
-        cy.viewport(1920,1080);
-        cy.visit('https://buger-eats.vercel.app');
-
-        cy.get('a[href="/deliver"]').click();
-        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas'); // checkpoint
-
         var deliver = {
             name: 'Vinicius Marinho',
             cpf: '000000141AA',
@@ -74,27 +48,12 @@ describe('Cadastro', () => {
             cnh: 'images/cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name);
-        cy.get('input[name="cpf"]').type(deliver.cpf);
-        cy.get('input[name="email"]').type(deliver.email);
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+        var signup = new SignupPage();
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
+        signup.go()
+        signup.fillForm(deliver);
+        signup.submit();
 
-        cy.get('input[name="address"]').should('have.value',deliver.address.street);
-        cy.get('input[name="district"]').should('have.value',deliver.address.district);
-        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state);
-
-        cy.get('input[name="address-number"]').type(deliver.address.number);
-        cy.get('input[name="address-details"]').type(deliver.address.details);
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click();
-
-        cy.get('input[accept^="image"]').attachFile(deliver.cnh);
-
-        cy.get('form button[type="submit"]').click();
-
-        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido');
+        signup.alertContentShouldBe('Oops! CPF inválido');
     });
 });
