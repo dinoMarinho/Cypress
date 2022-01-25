bb7f6f055aefd0abd9b4405455c956759c4cd54b
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-01-25 20:44:29 -0300

    Trabalhando com fixtures

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,57 +1,27 @@
-import SignupPage from '../pages/SignupPage';
-
+import signup from '../pages/SignupPage';
 
 describe('Cadastro', () => {
-    it('Usuário deve se tornar um deliver', () => {
-        var deliver = {
-            name: 'Vinicius Marinho',
-            cpf: '00000014141',
-            email: 'vinicius@vinicius.com',
-            whatsapp: '11123412345',
-            address: {
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Apto 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'images/cnh-digital.jpg'
-        }
 
-        var signup = new SignupPage();
+    beforeEach(function () { 
+        cy.fixture('deliver').then((deliver) => {
+            this.deliver = deliver;
+        })
+    });
+
+    it('Usuário deve se tornar um deliver', function () {
 
         signup.go();
-        signup.fillForm(deliver);
+        signup.fillForm(this.deliver.signup);
         signup.submit();
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
         signup.modalContentShouldBe(expectedMessage);
 
     });
-    it('CPF Incorreto', () => {
-        var deliver = {
-            name: 'Vinicius Marinho',
-            cpf: '000000141AA',
-            email: 'vinicius@vinicius.com',
-            whatsapp: '11123412345',
-            address: {
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Apto 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'images/cnh-digital.jpg'
-        }
-
-        var signup = new SignupPage();
+    it('CPF Incorreto', function() {
 
         signup.go()
-        signup.fillForm(deliver);
+        signup.fillForm(this.deliver.cpf_inv);
         signup.submit();
 
         signup.alertContentShouldBe('Oops! CPF inválido');
