e420bad27068a0e90aac57a477637795d0ffdb11
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-01-25 18:42:29 -0300

    Validando caso de erro e realizando refatoração para padrões de programação

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,47 +1,100 @@
 
 describe('Cadastro', () => {
-    it('Usuário deve se tornar um entregador', () => {
+    it('Usuário deve se tornar um deliver', () => {
         cy.viewport(1920,1080);
         cy.visit('https://buger-eats.vercel.app');
 
         cy.get('a[href="/deliver"]').click();
         cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas'); // checkpoint
 
-        var entregador = {
-            nome: 'Vinicius Marinho',
-            cpf: '11111111111',
+        var deliver = {
+            name: 'Vinicius Marinho',
+            cpf: '00000014141',
             email: 'vinicius@vinicius.com',
             whatsapp: '11123412345',
-            endereco: {
-                cep: '04534011',
-                rua: 'Rua Joaquim Floriano',
-                numero: '1000',
-                complemento: 'Apto 142',
-                bairro: 'Itaim Bibi',
-                cidade_uf: 'São Paulo/SP'
+            address: {
+                postalcode: '04534011',
+                street: 'Rua Joaquim Floriano',
+                number: '1000',
+                details: 'Apto 142',
+                district: 'Itaim Bibi',
+                city_state: 'São Paulo/SP'
             },
-            metodo_entrega: 'Moto',
+            delivery_method: 'Moto',
             cnh: 'images/cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(entregador.nome);
-        cy.get('input[name="cpf"]').type(entregador.cpf);
-        cy.get('input[name="email"]').type(entregador.email);
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+        cy.get('input[name="name"]').type(deliver.name);
+        cy.get('input[name="cpf"]').type(deliver.cpf);
+        cy.get('input[name="email"]').type(deliver.email);
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep);
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
         cy.get('input[type=button][value="Buscar CEP"]').click();
 
-        cy.get('input[name="address"]').should('have.value',entregador.endereco.rua);
-        cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro);
-        cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf);
+        cy.get('input[name="address"]').should('have.value',deliver.address.street);
+        cy.get('input[name="district"]').should('have.value',deliver.address.district);
+        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state);
 
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero);
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
+        cy.get('input[name="address-number"]').type(deliver.address.number);
+        cy.get('input[name="address-details"]').type(deliver.address.details);
 
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click();
+        cy.contains('.delivery-method li', deliver.delivery_method).click();
 
-        cy.get('input[accept^="image"]').attachFile(entregador.cnh);
+        cy.get('input[accept^="image"]').attachFile(deliver.cnh);
 
+        cy.get('form button[type="submit"]').click();
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+
+        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage);
+
+    });
+    it('CPF Incorreto', () => {
+        cy.viewport(1920,1080);
+        cy.visit('https://buger-eats.vercel.app');
+
+        cy.get('a[href="/deliver"]').click();
+        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas'); // checkpoint
+
+        var deliver = {
+            name: 'Vinicius Marinho',
+            cpf: '000000141AA',
+            email: 'vinicius@vinicius.com',
+            whatsapp: '11123412345',
+            address: {
+                postalcode: '04534011',
+                street: 'Rua Joaquim Floriano',
+                number: '1000',
+                details: 'Apto 142',
+                district: 'Itaim Bibi',
+                city_state: 'São Paulo/SP'
+            },
+            delivery_method: 'Moto',
+            cnh: 'images/cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(deliver.name);
+        cy.get('input[name="cpf"]').type(deliver.cpf);
+        cy.get('input[name="email"]').type(deliver.email);
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
+        cy.get('input[type=button][value="Buscar CEP"]').click();
+
+        cy.get('input[name="address"]').should('have.value',deliver.address.street);
+        cy.get('input[name="district"]').should('have.value',deliver.address.district);
+        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state);
+
+        cy.get('input[name="address-number"]').type(deliver.address.number);
+        cy.get('input[name="address-details"]').type(deliver.address.details);
+
+        cy.contains('.delivery-method li', deliver.delivery_method).click();
+
+        cy.get('input[accept^="image"]').attachFile(deliver.cnh);
+
+        cy.get('form button[type="submit"]').click();
+
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido');
     });
 });
