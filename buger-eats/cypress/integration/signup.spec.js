a0896171f43375f036621a16b7e3c3aceb5fe34f
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-01-25 21:48:01 -0300

    Implementando Factory no projeto

--- buger-eats/cypress/integration/signup.spec.js
+++ buger-eats/cypress/integration/signup.spec.js
@@ -1,17 +1,20 @@
 import signup from '../pages/SignupPage';
+import signupFactory from '../factories/SignupFactory'; 
 
 describe('Signup', () => {
 
-    beforeEach(function () { 
-        cy.fixture('deliver').then((deliver) => {
-            this.deliver = deliver;
-        })
-    });
+    // beforeEach(function () { 
+    //     cy.fixture('deliver').then((deliver) => {
+    //         this.deliver = deliver;
+    //     })
+    // });
 
     it('User should be deliver', function () {
 
+        var deliver = signupFactory.deliver();
+
         signup.go();
-        signup.fillForm(this.deliver.signup);
+        signup.fillForm(deliver);
         signup.submit();
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
@@ -20,10 +23,27 @@ describe('Signup', () => {
     });
     it('Incorrect document ', function() {
 
+        var deliver = signupFactory.deliver();
+
+        deliver.cpf = '000000141AA';
+
         signup.go()
-        signup.fillForm(this.deliver.cpf_inv);
+        signup.fillForm(deliver);
         signup.submit();
 
         signup.alertContentShouldBe('Oops! CPF inválido');
+    });
+
+    it('Incorrect email ', function() {
+
+        var deliver = signupFactory.deliver();
+
+        deliver.email = 'teste.com.br';
+
+        signup.go()
+        signup.fillForm(deliver);
+        signup.submit();
+
+        signup.alertContentShouldBe('Oops! Email com formato inválido.');
     });
 });
